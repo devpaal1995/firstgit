47ea22e8aa844c1caa9a09d01c20838f8ed4fb2b
devpaal1995 <95770426+devpaal1995@users.noreply.github.com>
2022-05-24 18:30:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+ let screen= ducument.getElementByid('screen');
+ buttons = ducument.querySelsctorAll('button');
+ let screenValue = '';
+ for(item of buttons);
+ item.addEventListener('click' ,(e)=>{
+     buttonText = e.target.innerText;
+    console.log('button text is ', buttonText);
+ })
+
+ if (buttonText=='X'){
+     buttonText = '*';
+     screenValue += buttonText;
+     screen.value = screenValue;
+    
+ }
+ else if (buttonText =='C'){
+    screen.value = "";  
+ }
+
+ else if (buttonText == '='){
+    screen.value = eval(screenValue);
+ }
+
+else{
+   screenValue += buttonText;
+   screen.value = screenValue;
+
+
+
+}
+
